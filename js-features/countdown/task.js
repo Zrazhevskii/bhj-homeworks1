51ee15f57170ab785bd6ed59440a11a76d83ef65
Zrazhevskii <vbn-134@yandex.ru>
2023-04-18 13:40:44 +0300

    корректировка ссылки на скачивание файла

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,7 +4,7 @@ const time = function() {
     if (text.textContent == 0) {
         alert('Вы победили в конкурсе!');
         clearInterval(intervalId);
-        document.location.assign('https://netology.ru/?utm_source=yandex&utm_medium=cpc&utm_campaign=brand_all_ou_ya_retarget_abtest_rassrochka_tomi_test&utm_content=13783431871&utm_term=%D0%BD%D0%B5%D1%82%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F&_openstat=ZGlyZWN0LnlhbmRleC5ydTs4NTQ5MzE5NTsxMzc4MzQzMTg3MTt5YW5kZXgucnU6cHJlbWl1bQ&yclid=8773369652516225023');
+        document.location.assign('https://blank-dogovor-kupli-prodazhi.com/files/%D0%94%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%20%D0%BA%D1%83%D0%BF%D0%BB%D0%B8-%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B8%20%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8F%202020.docx');
     }
 }
 
