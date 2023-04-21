59a5930b78d4020837d03a6acfef8d8da047433e
Zrazhevskii <vbn-134@yandex.ru>
2023-04-21 17:03:10 +0300

    написание функции

---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const main = document.getElementById('modal_main');
+const success = document.getElementById('modal_success');
+
+const closes = document.getElementsByClassName('modal__close');
+const show = document.getElementsByClassName('show-success');
+const btn_success = document.getElementsByClassName('btn_success');
+
+main.className = 'modal modal_active';
+
+function play() {
+    for (let i = 0; i < closes.length; i++)
+        closes[i].onclick = () => {
+            closes[i].closest('.modal').className = 'modal';
+            if (closes[i] === show[0])
+            success.className = 'modal modal_active';
+    }
+}
+
+play()
+        
