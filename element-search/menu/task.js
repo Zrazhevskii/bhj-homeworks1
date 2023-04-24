69afbac2ac96c01371ca18c133b855e3f7a87559
Zrazhevskii <vbn-134@yandex.ru>
2023-04-24 15:00:24 +0300

    дописание функции forEach, onclick

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,23 @@
 'use strict'
 
 const menu_link = document.getElementsByClassName('menu__link');
-
 let list_link = Array.from(menu_link);
 
-list_link.forEach((p) => {
-    console.log(p.textContent)
+list_link.forEach(item => {
+    item.onclick = function () {
+        let linkUl = item.parentElement.querySelector('ul');
+        let linkActive = item.closest('.menu_main').querySelector('.menu_active');
+
+        if (linkUl.classList.contains('menu_active')) {
+            linkActive.classList.remove('menu_active');
+        } else {
+            if(linkActive) {
+                linkActive.classList.remove('menu_active');
+                linkUl.classList.add('menu_active')
+            } else {
+                linkUl.classList.add('menu_active')
+            }
+        }
+        return false;
+    };
 })
