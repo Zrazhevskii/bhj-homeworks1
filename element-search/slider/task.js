eeba55e4370358822046e455ecad37cd7b80e00f
Zrazhevskii <vbn-134@yandex.ru>
2023-04-24 20:40:33 +0300

    коррекция

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,68 +1,11 @@
 'use strict'
 
-// let elSlidArrow = document.getElementsByClassName('slider__arrow');
-// let itemsImg = document.querySelectorAll('.slider__item');
-// let elDot = document.querySelectorAll('.slider__dot');
-// let indexImg = 0;
-
-// for (let i = 0; i < elSlidArrow.length; i++) {
-// 	elSlidArrow[i].onclick = changeSlide;
-// }
-
-// for (let i = 0; i < elDot.length; i++) {
-// 	elDot[i].onclick = () => {dotChangeSlide(i)};
-// }
-
-// function changeSlide (event) {
-// 	let classSlideArrow = event.target.classList;
-	
-// 	itemsImg[indexImg].classList.remove('slider__item_active');
-
-// 	if(classSlideArrow.contains('slider__arrow_prev')) {
-
-// 		if (indexImg == 0) {
-// 			indexImg = itemsImg.length - 1;
-// 		} else {
-// 			indexImg = indexImg - 1;
-// 		}
-// 	}
-
-// 	if(classSlideArrow.contains('slider__arrow_next')) {
-
-// 		if (indexImg == itemsImg.length - 1) {
-// 			indexImg = 0;
-// 		} else {
-// 			indexImg = indexImg + 1;
-// 		}
-// 	}
-
-// 	itemsImg[indexImg].classList.add('slider__item_active');
-// }
-
-// function dotChangeSlide(i) {
-// 	itemsImg[indexImg].classList.remove('slider__item_active');
-// 	itemsImg[i].classList.add('slider__item_active')
-// 	indexImg = i;
-// }
-
 const slidItem = document.querySelectorAll('.slider__item');
 const slidArrow = document.querySelectorAll('.slider__arrow');
 const slidPrev = document.querySelector('.slider__arrow_prev');
 const slidNext = document.querySelector('.slider__arrow_next');
 
-// console.log(slidItem[4])
-// console.log(slidNext)
-// console.log(slidArrow[1])
-// console.log(sliderItem[2])
-// const maxIndex = 5;
 let n = 0;
-
-// slidItem.forEach(item => {
-//     slidNext.onclick = () => {
-//         item.classList.toggle('slider__item_active')
-//     };
-// })
-
 
 
 for (let i = 0; i < slidArrow.length; i++) {
@@ -72,7 +15,6 @@ for (let i = 0; i < slidArrow.length; i++) {
 }
 
 function changeSlide(item) {
-    // console.log(item)
     slidItem[n].classList.remove('slider__item_active');
     
     if ((n == slidItem.length - 1) && (item == slidNext)) {
