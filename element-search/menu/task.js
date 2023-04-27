53f7bf8a2056fc8a27961337bdc4ba2db01eb983
Zrazhevskii <vbn-134@yandex.ru>
2023-04-27 13:00:19 +0300

    исправление ошибок, дописание функции

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,23 +1,26 @@
 'use strict'
 
-const menu_link = document.getElementsByClassName('menu__link');
+let menu_link = document.getElementsByClassName('menu__link');
 let list_link = Array.from(menu_link);
 
-list_link.forEach(item => {
-    item.onclick = function () {
-        let linkUl = item.parentElement.querySelector('ul');
-        let linkActive = item.closest('.menu_main').querySelector('.menu_active');
+list_link.forEach((item) => {
+	item.onclick = () => {
 
-        if (linkUl.classList.contains('menu_active')) {
-            linkActive.classList.remove('menu_active');
-        } else {
-            if(linkActive) {
-                linkActive.classList.remove('menu_active');
-                linkUl.classList.add('menu_active');
-            } else {
-                linkUl.classList.add('menu_active');
-            }
-        }
-        return false;
-    }
-})
+	let linkUl = item.parentElement.querySelector('ul');
+
+	if(linkUl) {
+		let disActive = linkUl.className.includes('menu_active');
+		let linkActive = item.closest('.menu_main').querySelector('.menu_active');
+
+		if(linkActive) {
+			linkActive.classList.remove('menu_active');
+		}
+		if (disActive) {
+			linkUl.classList.remove('menu_active');
+		} else {
+			linkUl.classList.add('menu_active');
+		}
+	}
+	return false;
+	}
+});
