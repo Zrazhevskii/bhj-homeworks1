d4c3f0d3cbf283d485e26ae6b979d9a1bcf3bd34
Zrazhevskii <vbn-134@yandex.ru>
2023-05-03 18:58:04 +0300

    коррекция

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,28 +1,24 @@
-'use strict'
+"use strict";
 
-const rotatorElement = Array.from(document.querySelectorAll('.rotator__case'));
-let index = rotatorElement.findIndex((value) => value.classList.contains('rotator__case_active'));
-console.log(index);
-
-
+const rotatorElement = Array.from(document.querySelectorAll(".rotator__case"));
+let index = rotatorElement.findIndex((value) =>
+  value.classList.contains("rotator__case_active")
+);
 
 let timeId = setInterval(() => {
-    let dataSet = rotatorElement[index].dataset;
-    rotatorElement[index].classList.remove('rotator__case_active');
+  let dataSet = rotatorElement[index].dataset;
+  rotatorElement[index].classList.remove("rotator__case_active");
 
-    if (index == rotatorElement.length - 1) {
-        index = 0;
-        rotatorElement[index].classList.add('rotator__case_active');
-        rotatorElement[index].style.color = dataSet.color;
-        rotatorElement[index].style.speed = dataSet.speed;
-    } else {
-        rotatorElement[index + 1].classList.add('rotator__case_active');
-        rotatorElement[index + 1].style.color = dataSet.color;
-        rotatorElement[index + 1].style.speed = dataSet.speed;
+  if (index == rotatorElement.length - 1) {
+    index = 0;
+    rotatorElement[index].classList.add("rotator__case_active");
+    rotatorElement[index].style.color = dataSet.color;
+    rotatorElement[index].style.speed = dataSet.speed;
+  } else {
+    rotatorElement[index + 1].classList.add("rotator__case_active");
+    rotatorElement[index + 1].style.color = dataSet.color;
+    rotatorElement[index + 1].style.speed = dataSet.speed;
 
-        index++;
-    }
-    
+    index++;
+  }
 }, 1000);
-
-
