b60d4654bd3ef72025aaea8d7b03f9c6c3bb4913
Zrazhevskii <vbn-134@yandex.ru>
2023-05-07 13:01:03 +0300

    коррекция в соответствии с рекомендациями

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,24 +1,29 @@
 "use strict";
 
 const rotatorElement = Array.from(document.querySelectorAll(".rotator__case"));
-let index = rotatorElement.findIndex((value) =>
-  value.classList.contains("rotator__case_active")
-);
+
 
 let timeId = setInterval(() => {
-  let dataSet = rotatorElement[index].dataset;
-  rotatorElement[index].classList.remove("rotator__case_active");
+  let index = rotatorElement.findIndex((value) =>
+  value.classList.contains("rotator__case_active")
+  );
 
-  if (index == rotatorElement.length - 1) {
-    index = 0;
+  let dataSet = rotatorElement[index].dataset;
+  
+  rotatorElement[index].classList.remove("rotator__case_active");
+  let nextElement = rotatorElement[index].nextElementSibling
+  
+  if (nextElement === null) {
+    index = 0;    
     rotatorElement[index].classList.add("rotator__case_active");
     rotatorElement[index].style.color = dataSet.color;
     rotatorElement[index].style.speed = dataSet.speed;
   } else {
-    rotatorElement[index + 1].classList.add("rotator__case_active");
-    rotatorElement[index + 1].style.color = dataSet.color;
-    rotatorElement[index + 1].style.speed = dataSet.speed;
-
-    index++;
+    nextElement.classList.add("rotator__case_active")
+    nextElement.style.color = dataSet.color;
+    nextElement.style.speed = dataSet.speed;
   }
+  
+    index++;
+  
 }, 1000);
