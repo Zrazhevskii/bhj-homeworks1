59713282060ec7dab10c82694a6a4a8d928931d3
Zrazhevskii <vbn-134@yandex.ru>
2023-05-16 10:22:38 +0300

    правка кодстайла

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,42 +1,41 @@
-'use strict'
+"use strict";
 
 // const taskRemove = Array.from(document.querySelectorAll('.task__remove'))
-const taskList = document.querySelector('.tasks__list')
-const tasksInput = document.querySelector('.tasks__input')
-const tasks = document.querySelector('.tasks')
-const divInput = Array.from(document.querySelectorAll('.task'))
-const task = document.querySelector('.task')
+const taskList = document.querySelector(".tasks__list");
+const tasksInput = document.querySelector(".tasks__input");
+const tasks = document.querySelector(".tasks");
+const divInput = Array.from(document.querySelectorAll(".task"));
+const task = document.querySelector(".task");
 
-const taskAdd = document.querySelector('.tasks__add');
+const taskAdd = document.querySelector(".tasks__add");
 
-taskAdd.addEventListener('click', (evt) => {
-    createMessege(evt)
-})
+taskAdd.addEventListener("click", (evt) => {
+  createMessege(evt);
+});
 
-tasks.addEventListener('keydown', (evt) => {
-    if (evt.code === 'Enter') { 
-        createMessege(evt);
-    }  
-})
+tasks.addEventListener("keydown", (evt) => {
+  if (evt.code === "Enter") {
+    createMessege(evt);
+  }
+});
 
 function createMessege(evt) {
-    evt.preventDefault();
-    
-    if (tasksInput.value === '') {
-        return
-    } else {
-        const div = document.createElement("div");
-        div.className = 'task'
-        div.innerHTML += `<div class="task__title">${tasksInput.value}</div>
+  evt.preventDefault();
+
+  if (tasksInput.value === "") {
+    return;
+  } else {
+    const div = document.createElement("div");
+    div.className = "task";
+    div.innerHTML += `<div class="task__title">${tasksInput.value}</div>
                         <a href="#" class="task__remove">&times;</a>`;
-        taskList.prepend(div);
-        tasksInput.value = ''
-    }
-    
+    taskList.prepend(div);
+    tasksInput.value = "";
+  }
 }
 
-taskList.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains("task__remove")) {
-        evt.target.parentElement.remove();
-      }
-})
+taskList.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("task__remove")) {
+    evt.target.parentElement.remove();
+  }
+});
