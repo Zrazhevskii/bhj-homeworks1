9d6765fa6d62629360406b3a34a9a59909133a3d
Zrazhevskii <vbn-134@yandex.ru>
2023-05-29 14:47:26 +0300

    написание кода

---
+++ client-state/auth/task.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const signin = document.querySelector(".signin");
+const welcome = document.querySelector(".welcome");
+const btn = document.getElementById("signin__btn");
+const inputName = document.getElementsByName("login")[0];
+const inputPassword = document.getElementsByName("password")[0];
+const form = document.getElementById("signin__form");
+const userId = document.getElementById("user_id");
+const requestURL = "https://students.netoservices.ru/nestjs-backend/auth";
+
+if (localStorage.getItem("idUser")) {
+  signin.classList.remove("signin_active");
+  welcome.classList.add("welcome_active");
+  userId.textContent = `${localStorage.getItem("idUser")}`;
+}
+
+async function sendRequest(url, body) {
+  if (inputName.value == "" || inputPassword.value == "") {
+    alert("Поля должны быть заполнены!");
+    form.reset();
+  } else {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (response.ok) {
+      let text = await response.text();
+      text = JSON.parse(text);
+      show(text["user_id"]);
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+      form.reset();
+    }
+  }
+}
+
+function show(id) {
+  signin.classList.remove("signin_active");
+  welcome.classList.add("welcome_active");
+  userId.textContent = `${id}`;
+  localStorage.setItem("idUser", id);
+}
+
+btn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  let body = {
+    login: inputName.value,
+    password: inputPassword.value,
+  };
+
+  sendRequest(requestURL, body)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
