9bdd9ef246955d17e9d06fc198713f56ae6e6f27
Zrazhevskii <vbn-134@yandex.ru>
2023-05-29 14:51:18 +0300

    стилизация

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,24 +1,23 @@
-'use strict';
+"use strict";
 
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal__close");
+const allCookie = document.cookie.split(";");
 
-const modal = document.querySelector('.modal');
-const modalClose = document.querySelector('.modal__close');
-const  allCookie = document.cookie.split(';');
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   for (let i = 0; i < allCookie.length; i++) {
-    console.log(allCookie[i]); 
-    if (allCookie[i] === 'popup=close') {
-      modal.classList.remove('modal_active')
+    console.log(allCookie[i]);
+    if (allCookie[i] === "popup=close") {
+      modal.classList.remove("modal_active");
     } else {
-      modal.classList.add('modal_active')
+      modal.classList.add("modal_active");
     }
   }
-})
+});
 
-modalClose.addEventListener('click', () => {
-  modal.classList.remove('modal_active')
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("modal_active");
   let date = new Date(Date.now() + 86400e3);
   date = date.toUTCString();
-  document.cookie = 'popup=close; expires=' + date
-})
+  document.cookie = "popup=close; expires=" + date;
+});
