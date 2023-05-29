2bca31418a5f6ab1cd4dc8e454f09e32e61237b9
Zrazhevskii <vbn-134@yandex.ru>
2023-05-29 14:50:45 +0300

    стилизация

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,20 +1,19 @@
-'use strict'
+"use strict";
 
-let textarea = document.querySelector('textarea')
-let save = document.querySelector('.save')
-let clear = document.querySelector('.clear')
-let show = document.querySelector('.show')
+let textarea = document.querySelector("textarea");
+let save = document.querySelector(".save");
+let clear = document.querySelector(".clear");
+let show = document.querySelector(".show");
 
-textarea.value = localStorage.getItem('value')
+textarea.value = localStorage.getItem("value");
 
-save.addEventListener('click', function(){
-    localStorage.setItem('value', textarea.value)
-})
+save.addEventListener("click", function () {
+  localStorage.setItem("value", textarea.value);
+});
 
-clear.addEventListener('click', function(){
-    textarea.value = ''
-    localStorage.removeItem('value')
-    localStorage.removeItem('value1')
-})
+clear.addEventListener("click", function () {
+  textarea.value = "";
+  localStorage.removeItem("value");
+});
 
-console.log(localStorage.getItem('value'));
+console.log(localStorage.getItem("value"));
